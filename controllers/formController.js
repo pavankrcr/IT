94f5f303e38412ed5e4c22bba376fa50622b5bb0
pavank <mail@pavank.in>
2023-12-27 11:37:56 +0530

    Changed db to postgressql

--- controllers/formController.js
+++ controllers/formController.js
@@ -22,7 +22,7 @@ const submitForm = async (req, res) => {
 
     // Save PDF file to disk
     let pdfPath;
-    if (req.file ) {
+    if (req.file) {
       pdfPath = `uploads/${req.body.id}`;
       await fs.rename(req.file.path, pdfPath);
       console.log(pdfPath);
@@ -40,8 +40,26 @@ const submitForm = async (req, res) => {
       subject: 'Form Submission Confirmation',
       text: `Thank you for submitting the form, ${name}!`,
       html: `<div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
-    <h2 style="color: #333; text-align: center;">Thank you for submitting the form, ${name}!</h2>
-    <!-- ... rest of the HTML ... -->
+      <h2 style="color: #333; text-align: center;">Thank you for submitting the form, ${name}!</h2>
+      <table style="width: 100%; border-collapse: collapse; margin-top: 20px; border: 1px solid #ddd;">
+        <tr style="background-color: #4caf50; color: #fff; text-align: left;">
+          <th style="padding: 10px; border: 1px solid #ddd;">Field</th>
+          <th style="padding: 10px; border: 1px solid #ddd;">Value</th>
+        </tr>
+        <tr>
+          <td style="padding: 10px; border: 1px solid #ddd;"><strong>ID</strong></td>
+          <td style="padding: 10px; border: 1px solid #ddd;">${id}</td>
+        </tr>
+        <tr>
+          <td style="padding: 10px; border: 1px solid #ddd;"><strong>Name</strong></td>
+          <td style="padding: 10px; border: 1px solid #ddd;">${name}</td>
+        </tr>
+        <tr>
+          <td style="padding: 10px; border: 1px solid #ddd;"><strong>Email</strong></td>
+          <td style="padding: 10px; border: 1px solid #ddd;">${email}</td>
+        </tr>
+      </table>
+      <p style="text-align: center; margin-top: 20px; font-size: 16px; color: #555;">Attached is the PDF file containing additional details.</p>
     </div>`,
       attachments: req.file ? [{ filename: req.file.originalname, path: pdfPath }] : [],
     };
--- models/userModel.js
+++ models/userModel.js
@@ -1,20 +1,39 @@
-const mysql = require('mysql');
-const { mysqlConfig } = require('../config/config');
+const { Pool } = require('pg');
+const { postgresConfig } = require('../config/config');
 
-const connection = mysql.createConnection(mysqlConfig);
+const pool = new Pool(postgresConfig);
 
-connection.connect((err) => {
+pool.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error('Error connecting to PostgreSQL:', err);
     return;
   }
-  console.log('Connected to MySQL database');
+  console.log('Connected to PostgreSQL database');
 });
 
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    empid INTEGER,
+    name TEXT,
+    email TEXT
+  );
+`
+
+// Execute the table creation query
+pool.query(createTableQuery, (err, result) => {
+  if (err) {
+    console.error('Error creating table:', err);
+  } else {
+    console.log('Table created or already exists');
+  }
+});
+
+
 const saveUser = (id, name, email, callback) => {
-  const insertQuery = 'INSERT INTO users (empid, name, email) VALUES (?, ?, ?)';
+  const insertQuery = 'INSERT INTO users (empid, name, email) VALUES ($1, $2, $3)';
   const insertValues = [id, name, email];
-  connection.query(insertQuery, insertValues, callback);
+  pool.query(insertQuery, insertValues, callback);
 };
 
 module.exports = {
