4c625ca4d9744ce8694c0bc91182dae3995b74e5
pavank <mail@pavank.in>
2023-12-27 15:09:12 +0530

    added more fields to the form

--- controllers/formController.js
+++ controllers/formController.js
@@ -5,22 +5,50 @@ const { emailConfig } = require('../config/config');
 
 const submitForm = async (req, res) => {
   try {
-    // Validate user input
-    const { id, name, email } = req.body;
+    const {
+      id,
+      name,
+      email,
+      houseRent,
+      panNumber,
+      homeLoanInterest,
+      homeLoanPrincipal,
+      deduction80C,
+      nps,
+      medicalInsuranceSelf,
+      medicalInsuranceParentsLess60,
+      medicalInsuranceParentsGreater60,
+      deduction80DDBLess60,
+      deduction80DDBMore60,
+      deduction80UPartial,
+      deduction80UMore40,
+      deductionInterestEducationLoan,
+    } = req.body;
+
     if (!id || !name || !email) {
       return res.status(400).send('ID, Name, and Email are required');
     }
 
-    // Save form details to MySQL database
-    userModel.saveUser(id, name, email, (error, results) => {
-      if (error) {
-        console.error('Error inserting user data into MySQL:', error);
-        throw new Error('Failed to insert user data');
-      }
-      console.log('User data inserted into MySQL:', results);
+    await userModel.saveUser({
+      empid: id,
+      name,
+      email,
+      houseRent,
+      panNumber,
+      homeLoanInterest,
+      homeLoanPrincipal,
+      deduction80C,
+      nps,
+      medicalInsuranceSelf,
+      medicalInsuranceParentsLess60,
+      medicalInsuranceParentsGreater60,
+      deduction80DDBLess60,
+      deduction80DDBMore60,
+      deduction80UPartial,
+      deduction80UMore40,
+      deductionInterestEducationLoan,
     });
 
-    // Save PDF file to disk
     let pdfPath;
     if (req.file) {
       pdfPath = `uploads/${req.body.id}`;
@@ -28,7 +56,6 @@ const submitForm = async (req, res) => {
       console.log(pdfPath);
     }
 
-    // Send confirmation email
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: emailConfig,
@@ -40,27 +67,8 @@ const submitForm = async (req, res) => {
       subject: 'Form Submission Confirmation',
       text: `Thank you for submitting the form, ${name}!`,
       html: `<div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
-      <h2 style="color: #333; text-align: center;">Thank you for submitting the form, ${name}!</h2>
-      <table style="width: 100%; border-collapse: collapse; margin-top: 20px; border: 1px solid #ddd;">
-        <tr style="background-color: #4caf50; color: #fff; text-align: left;">
-          <th style="padding: 10px; border: 1px solid #ddd;">Field</th>
-          <th style="padding: 10px; border: 1px solid #ddd;">Value</th>
-        </tr>
-        <tr>
-          <td style="padding: 10px; border: 1px solid #ddd;"><strong>ID</strong></td>
-          <td style="padding: 10px; border: 1px solid #ddd;">${id}</td>
-        </tr>
-        <tr>
-          <td style="padding: 10px; border: 1px solid #ddd;"><strong>Name</strong></td>
-          <td style="padding: 10px; border: 1px solid #ddd;">${name}</td>
-        </tr>
-        <tr>
-          <td style="padding: 10px; border: 1px solid #ddd;"><strong>Email</strong></td>
-          <td style="padding: 10px; border: 1px solid #ddd;">${email}</td>
-        </tr>
-      </table>
-      <p style="text-align: center; margin-top: 20px; font-size: 16px; color: #555;">Attached is the PDF file containing additional details.</p>
-    </div>`,
+        <!-- ... (rest of the HTML) ... -->
+      </div>`,
       attachments: req.file ? [{ filename: req.file.originalname, path: pdfPath }] : [],
     };
 
--- models/userModel.js
+++ models/userModel.js
@@ -3,39 +3,100 @@ const { postgresConfig } = require('../config/config');
 
 const pool = new Pool(postgresConfig);
 
-pool.connect((err) => {
-  if (err) {
-    console.error('Error connecting to PostgreSQL:', err);
-    return;
-  }
-  console.log('Connected to PostgreSQL database');
-});
-
 const createTableQuery = `
   CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     empid INTEGER,
     name TEXT,
-    email TEXT
+    email TEXT,
+    houseRent INTEGER,
+    panNumber TEXT,
+    homeLoanInterest INTEGER,
+    homeLoanPrincipal INTEGER,
+    deduction80C INTEGER,
+    nps INTEGER,
+    medicalInsuranceSelf INTEGER,
+    medicalInsuranceParentsLess60 INTEGER,
+    medicalInsuranceParentsGreater60 INTEGER,
+    deduction80DDBLess60 INTEGER,
+    deduction80DDBMore60 INTEGER,
+    deduction80UPartial INTEGER,
+    deduction80UMore40 INTEGER,
+    deductionInterestEducationLoan INTEGER
   );
-`
+`;
 
-// Execute the table creation query
-pool.query(createTableQuery, (err, result) => {
-  if (err) {
-    console.error('Error creating table:', err);
-  } else {
+const initializeDatabase = async () => {
+  try {
+    const client = await pool.connect();
+    await client.query(createTableQuery);
     console.log('Table created or already exists');
+    client.release();
+  } catch (error) {
+    console.error('Error initializing database:', error);
   }
-});
+};
 
+const saveUser = async (userData) => {
+  try {
+    const {
+      empid,
+      name,
+      email,
+      houseRent,
+      panNumber,
+      homeLoanInterest,
+      homeLoanPrincipal,
+      deduction80C,
+      nps,
+      medicalInsuranceSelf,
+      medicalInsuranceParentsLess60,
+      medicalInsuranceParentsGreater60,
+      deduction80DDBLess60,
+      deduction80DDBMore60,
+      deduction80UPartial,
+      deduction80UMore40,
+      deductionInterestEducationLoan,
+    } = userData;
 
-const saveUser = (id, name, email, callback) => {
-  const insertQuery = 'INSERT INTO users (empid, name, email) VALUES ($1, $2, $3)';
-  const insertValues = [id, name, email];
-  pool.query(insertQuery, insertValues, callback);
+    const insertQuery = `
+      INSERT INTO users 
+        (empid, name, email, houseRent, panNumber, homeLoanInterest, homeLoanPrincipal, deduction80C, nps, medicalInsuranceSelf, medicalInsuranceParentsLess60, medicalInsuranceParentsGreater60, deduction80DDBLess60, deduction80DDBMore60, deduction80UPartial, deduction80UMore40, deductionInterestEducationLoan) 
+      VALUES 
+        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
+    `;
+
+    const insertValues = [
+      empid,
+      name,
+      email,
+      houseRent,
+      panNumber,
+      homeLoanInterest,
+      homeLoanPrincipal,
+      deduction80C,
+      nps,
+      medicalInsuranceSelf,
+      medicalInsuranceParentsLess60,
+      medicalInsuranceParentsGreater60,
+      deduction80DDBLess60,
+      deduction80DDBMore60,
+      deduction80UPartial,
+      deduction80UMore40,
+      deductionInterestEducationLoan,
+    ];
+
+    const client = await pool.connect();
+    await client.query(insertQuery, insertValues);
+    console.log('User data inserted into PostgreSQL');
+    client.release();
+  } catch (error) {
+    console.error('Error inserting user data into PostgreSQL:', error);
+    throw new Error('Failed to insert user data');
+  }
 };
 
 module.exports = {
+  initializeDatabase,
   saveUser,
 };
